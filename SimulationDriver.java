9a65cb00ceaca184e3b709544483a1b8303f35e6
lpapal03 <lpapal03@ucy.ac.cy>
2020-04-11 22:37:40 +0300

    Added inputs in Driver

--- SimulationDriver.java
+++ SimulationDriver.java
@@ -12,47 +12,76 @@ public class SimulationDriver {
 
 		int width = 0;
 		int length = 0;
-		int time=0;
+		int time = 0;
+		int people = 0;
+		boolean fatal;
 		String input;
 
-		while (error) {
+		do {
 			error = false;
-			System.out.print("Give the length: ");	
-			input =scan.nextLine();
+			System.out.print("Give the length: ");
+			input = scan.nextLine();
 			try {
-				length=Integer.parseInt(input);
+				length = Integer.parseInt(input);
 			} catch (NumberFormatException e) {
-				System.out.print("Give a positive integer: ");
+				System.out.println("Must be a positive integer");
 				error = true;
 			}
-		}
-		
-		error=true;
-		
-		while (error) {
+		} while (error);
+
+		do {
 			error = false;
-			System.out.print("Give the width: ");	
-			input =scan.nextLine();
+			System.out.print("Give the width: ");
+			input = scan.nextLine();
 			try {
-				width=Integer.parseInt(input);
+				width = Integer.parseInt(input);
 			} catch (NumberFormatException e) {
-				System.out.print("Give a positive integer: ");
+				System.out.println("Must be a positive integer");
 				error = true;
 			}
-		}
-		
-		error=true;
-		while (error) {
+		} while (error);
+
+		do {
 			error = false;
-			System.out.print("Give the time (in minutes): ");	
-			input =scan.nextLine();
+			System.out.print("Give the time amount of people: ");
+			input = scan.nextLine();
 			try {
-				time=Integer.parseInt(input);
+				people = Integer.parseInt(input);
+				if (people > length * width) {
+					System.out.println("Cannot have more people than available spaces");
+					error = true;
+				}
 			} catch (NumberFormatException e) {
-				System.out.print("Give a positive integer: ");
+				System.out.println("Must be a positive integer");
 				error = true;
 			}
-		}
+		} while (error);
+
+		do {
+			error = false;
+			System.out.print("Give the time (in minutes): ");
+			input = scan.nextLine();
+			try {
+				time = Integer.parseInt(input);
+			} catch (NumberFormatException e) {
+				System.out.println("Must be a positive integer");
+				error = true;
+			}
+		} while (error);
+		
+		do {
+			error = false;
+			System.out.print("Is the disease fatal? ");
+			input = scan.nextLine();
+			try {
+				fatal = Boolean.parseBoolean(input);
+			} catch (NumberFormatException e) {
+				System.out.println("Must be a true or false");
+				error = true;
+			}
+		} while (error);
+
+		System.out.println("Created a board of dimentions ");
 
 		Room room = new Room(width, length);
 	}
