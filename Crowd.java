d92e031e2ef267f7df3269ea4b4f9429d6a8321e
Taoufik Sousak <tsousa01@ucy.ac.cy>
2020-04-15 18:28:48 +0300

    seperated move method to move and updateStatus

--- Crowd.java
+++ Crowd.java
@@ -1,5 +1,13 @@
 package tsousa01.hw5;
 
+/**
+ * The class responsible for connecting the humans wit the room, has move method
+ * to move all the humans and updateStatus to update the status of them.
+ * 
+ * 
+ * @author Taoufik Sousak, Loukas Papalazarou
+ *
+ */
 public class Crowd {
 
 	private Human[] humans;
@@ -9,8 +17,23 @@ public class Crowd {
 	private static double inf;
 	private int duration;
 
-	public Crowd(int duration, int width, int length, int people, int howLikelyToMove, int sick, 
-			int fatal, int infectius, int caref) {
+	/**
+	 * The only constructor, decides where to place each human, creates them and
+	 * places them.
+	 * 
+	 * @param duration        how long it takes for someone to recover and for a
+	 *                        room to get disinfected
+	 * @param width           the width of the grid
+	 * @param length          the length of the grid
+	 * @param people          how many people there will be in general
+	 * @param howLikelyToMove a percentage of how likely each human is to move
+	 * @param sick            how many people are initially sick
+	 * @param fatal           how fatal is the virus %
+	 * @param infectius       how inectius is the virus
+	 * @param caref           how many people take protective measures
+	 */
+	public Crowd(int duration, int width, int length, int people, int howLikelyToMove, int sick, int fatal,
+			int infectius, int caref) {
 		room = new Room(width, length, duration, infectius);
 		humans = new Human[people];
 		this.howLikelyToMove = (double) howLikelyToMove / 100;
@@ -19,31 +42,36 @@ public class Crowd {
 		this.fatal = (double) fatal / 100;
 		this.inf = (double) infectius / 100;
 		this.duration = duration;
-
+		// decide where to place human
 		for (int i = 0; i < people; i++) {
 			do {
 				xpos = (int) (Math.random() * ((width)));
 				ypos = (int) (Math.random() * ((length)));
 
-			} while (room.isOccupied(ypos, xpos) != 0); // ypos <--> xpos because the methods in Room work with (int y,
-														// int
-														// x)
+			} while (room.isOccupied(ypos, xpos) != 0);
+
+			// create sick humans
 			if (i < sick) {
-				
+
 				humans[i] = new InfectedHuman(xpos, ypos, duration);
 				room.occupy(ypos, xpos, 2);
 			} else {
-				boolean msrs=false;
-				if(i-sick<caref)
-					msrs=true;
+				boolean msrs = false;
+				// put masks on specified amount
+				if (i - sick < caref)
+					msrs = true;
 				humans[i] = new HealthyHuman(xpos, ypos, msrs);
-				room.occupy(ypos, xpos, 1); // ypos <--> xpos because the methods in Room work with (int y, int x)}
+				room.occupy(ypos, xpos, 1);
 			}
 			room.drawGrid();
 
 		}
 	}
 
+	/**
+	 * moves every human individaully, then calls update status to update their
+	 * status and draws the grid
+	 */
 	public void move() {
 
 		for (int i = 0; i < humans.length; i++) {
@@ -51,11 +79,13 @@ public class Crowd {
 			int newxpos = humans[i].getXpos();
 			int newypos = humans[i].getYpos();
 			String direction = null;
+			// decide if human will move
 			if (Randomizer.getBoolean(howLikelyToMove)) {
 				room.occupy(humans[i].getYpos(), humans[i].getXpos(), 0);
 				room.setMeasures(newypos, newxpos, false);
+				// choose new available position
 				do {
-
+					// check if trapped
 					if (noPossibleMove(humans[i].getXpos(), humans[i].getYpos()))
 						break;
 
@@ -63,13 +93,9 @@ public class Crowd {
 					newypos = humans[i].getYpos();
 
 					direction = Randomizer.getDirection();
-					// System.out.println("direction: " + direction + " contains u: " + // only for
-					// testing
-					// direction.contains("u"));
-					if (direction.contains("u") && humans[i].getYpos() != room.getLength() - 1) {
+
+					if (direction.contains("u") && humans[i].getYpos() != room.getLength() - 1)
 						newypos++;
-						// System.out.println("ITS AN UP");
-					}
 					if (direction.contains("d") && humans[i].getYpos() != 0)
 						newypos--;
 					if (direction.contains("r") && humans[i].getXpos() != room.getWidth() - 1)
@@ -77,36 +103,11 @@ public class Crowd {
 					if (direction.contains("l") && humans[i].getXpos() != 0)
 						newxpos--;
 				} while (room.isOccupied(newypos, newxpos) != 0);
+
+				humans[i].moveTo(newxpos, newypos);
+
+				this.updateStatus(i, newxpos, newypos);
 			}
-			humans[i].moveTo(newxpos, newypos);
-
-			if (humans[i] instanceof InfectedHuman)
-				if (humans[i].getTimeLeft() == 0)
-					if (!Randomizer.getBoolean(fatal))
-						humans[i] = new RecoveredHuman(humans[i].getXpos(), humans[i].getYpos());
-					else {
-						humans[i] = new DeceasedHuman(humans[i].getXpos(), humans[i].getYpos());
-						room.occupy(newypos, newypos, 0);
-					}
-
-			if (humans[i] instanceof HealthyHuman) {	
-				room.occupy(newypos, newxpos, 1);
-				
-				if (possibleInfection(humans[i].getXpos(), humans[i].getYpos(), humans[i].takesMeasures()))
-					if(humans[i].takesMeasures()) 
-						humans[i] = new InfectedHuman(humans[i].getXpos(), humans[i].getYpos(), duration, true);
-					else
-						humans[i] = new InfectedHuman(humans[i].getXpos(), humans[i].getYpos(), duration);
-
-				
-
-			} else if (humans[i] instanceof InfectedHuman)
-				room.occupy(newypos, newxpos, 2);
-			else if (humans[i] instanceof RecoveredHuman)
-				room.occupy(newypos, newxpos, 3);
-
-			if(humans[i].takesMeasures())
-				room.setMeasures(newypos, newxpos, true);
 		}
 		room.drawGrid();
 
@@ -115,6 +116,42 @@ public class Crowd {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * infects people that should get infected, recovers people that should recover
+	 * and kills people that should die accordingly.
+	 * 
+	 * @param i
+	 * @param newxpos
+	 * @param newypos
+	 */
+	public void updateStatus(int i, int newxpos, int newypos) {
+		if (humans[i] instanceof InfectedHuman)
+			if (humans[i].getTimeLeft() == 0)
+				if (!Randomizer.getBoolean(fatal))
+					humans[i] = new RecoveredHuman(humans[i].getXpos(), humans[i].getYpos());
+				else {
+					humans[i] = new DeceasedHuman(humans[i].getXpos(), humans[i].getYpos());
+					room.occupy(newypos, newxpos, 0);
+				}
+
+		if (humans[i] instanceof HealthyHuman) {
+			room.occupy(newypos, newxpos, 1);
+
+			if (possibleInfection(humans[i].getXpos(), humans[i].getYpos(), humans[i].takesMeasures()))
+				if (humans[i].takesMeasures())
+					humans[i] = new InfectedHuman(humans[i].getXpos(), humans[i].getYpos(), duration, true);
+				else
+					humans[i] = new InfectedHuman(humans[i].getXpos(), humans[i].getYpos(), duration);
+
+		} else if (humans[i] instanceof InfectedHuman)
+			room.occupy(newypos, newxpos, 2);
+		else if (humans[i] instanceof RecoveredHuman)
+			room.occupy(newypos, newxpos, 3);
+
+		if (humans[i].takesMeasures())
+			room.setMeasures(newypos, newxpos, true);
 	}
 
 	public static boolean possibleInfection(int x, int y, boolean measures) {
@@ -174,9 +211,9 @@ public class Crowd {
 	}
 
 	public static boolean roomTransmition(int x, int y, boolean measures) {
-		double chance=inf/2; // because its less likely to get virus from room than human
-		if(measures)
-			chance/=2;	//protective measures lower chance of infection
+		double chance = inf / 2; // because its less likely to get virus from room than human
+		if (measures)
+			chance /= 2; // protective measures lower chance of infection
 		if (room.isInfected(y, x) > 0)
 			if (Randomizer.getBoolean(chance))
 				return true;
@@ -184,10 +221,10 @@ public class Crowd {
 	}
 
 	public static boolean humanTransmition(int x, int y, boolean measures) {
-		
-		double chance=inf;
-		if(measures)
-			chance/=2; //protective measures lower chance of infection
+
+		double chance = inf;
+		if (measures)
+			chance /= 2; // protective measures lower chance of infection
 		try {
 			if (room.isOccupied(y - 1, x + 1) == 2)
 				if (Randomizer.getBoolean(chance))
