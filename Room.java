ca15c3bc87d060a7128bc20daf10749de44bbe3e
Taoufik Sousak <tsousa01@ucy.ac.cy>
2020-04-29 14:11:23 +0300

    added assignPorts method to Room

--- Room.java
+++ Room.java
@@ -11,68 +11,96 @@ public class Room {
 	private int duration;
 	private double inf;
 	private boolean measures[][];
-	
-	private int port[][];	//0=not an port, otherwise number represents to which city it leads
+
+	private int port[][]; // 0=not an port, otherwise number represents to which city it leads
 
 	/**
 	 * 
-	 * @param width of grid
-	 * @param length of grid
-	 * @param duration is the amount of time required for a person to heal 
-	 * @param infectious how likely is for people to contract the virus from other infected people or spaces
+	 * @param width      of grid
+	 * @param length     of grid
+	 * @param duration   is the amount of time required for a person to heal
+	 * @param infectious how likely is for people to contract the virus from other
+	 *                   infected people or spaces
 	 */
 	public Room(int width, int length, int duration, int infectious) {
 		this.width = width;
 		this.length = length;
 		occupied = new int[length][width];
 		infected = new int[length][width];
-		
+
 		port = new int[length][width];
-		
-		measures= new boolean[length][width];
-		this.duration=duration;
-		this.inf=(double) infectious/100;
-		inf/=3;		//less likely than human transmission
+
+		measures = new boolean[length][width];
+		this.duration = duration;
+		this.inf = (double) infectious / 100;
+		inf /= 3; // less likely than human transmission
 		for (int i = 0; i < length; i++) {
 			for (int j = 0; j < width; j++) {
 				occupied[i][j] = 0;
 				infected[i][j] = 0;
-				measures[i][j]=false;
+				measures[i][j] = false;
 			}
 		}
-		//setup of grid
+		// setup of grid
 		this.gridSetup();
 		this.drawGrid();
 	}
-	
-	public void assignPorts(int howManyPorts) {
-		
-		for (int i=0; i<howManyPorts; i++)
-			
-		
-		
+
+	public void assignPorts(int howManyPorts, int howManyRooms, int thisRoomNumber) {
+
+		int xpos = 0, ypos = 0;
+		int whereTo = 0;
+
+		do { // decide where port leads
+			whereTo = Randomizer.getInteger(howManyRooms);
+		} while (whereTo == thisRoomNumber); // repeat if port leads to same city
+
+		do {
+			// decide on which side of room the port will be
+			String side = "error";
+			while (side.length() > 1)
+				side = Randomizer.getDirection();
+
+			// decide where on said side port will be
+			if (side == "u" || side == "d") {
+				xpos = Randomizer.getInteger(width);
+				if (side == "u")
+					ypos = length;
+				else
+					ypos = 0;
+			} else {
+				ypos = Randomizer.getInteger(length);
+				if (side == "l")
+					xpos = 0;
+				else
+					xpos = width;
+			}
+		} while (this.port[xpos][ypos] != 0);// repeat if a port is already at this position
+
+	}
+
+	/**
+	 * 
+	 * @param y        position
+	 * @param x        position
+	 * @param measures sets whether the person on that position is taking protective
+	 *                 measures
+	 */
+	public void setMeasures(int y, int x, boolean measures) {
+		this.measures[y][x] = measures;
 	}
 
 	/**
 	 * 
 	 * @param y position
 	 * @param x position
-	 * @param measures sets whether the person on that position is taking protective measures
-	 */
-	public void setMeasures(int y, int x, boolean measures) {
-		this.measures[y][x]=measures;
-	}
-	
-	/**
-	 * 
-	 * @param y position
-	 * @param x position
-	 * @return returns whether the person on that position is taking protective measures
+	 * @return returns whether the person on that position is taking protective
+	 *         measures
 	 */
 	public boolean takesMeasures(int y, int x) {
 		return measures[y][x];
 	}
-	
+
 	/**
 	 * 
 	 * @return the length
@@ -102,7 +130,7 @@ public class Room {
 	/**
 	 * 
 	 * @param y position
-	 * @param x position 
+	 * @param x position
 	 * @return true if the space on that position has a person on it
 	 */
 	public int isOccupied(int y, int x) {
@@ -114,7 +142,7 @@ public class Room {
 	 * @param y position
 	 * @param x position
 	 * 
-	 * Sets specified position to be infected
+	 *          Sets specified position to be infected
 	 */
 	public void infect(int y, int x) {
 		infected[y][x] = duration;
@@ -125,16 +153,16 @@ public class Room {
 	 * @param y position
 	 * @param x position
 	 * 
-	 * Sets specified position to not be infected
+	 *          Sets specified position to not be infected
 	 */
 	public void disinfect(int y, int x) {
 		infected[y][x]--;
 	}
-	
+
 	/**
 	 * 
-	 * @param y position
-	 * @param x position
+	 * @param y      position
+	 * @param x      position
 	 * @param status is set to the specified position
 	 */
 	public void occupy(int y, int x, int status) {
@@ -153,25 +181,25 @@ public class Room {
 
 	/**
 	 * 
-	 * @param y position
-	 * @param x position
+	 * @param y    position
+	 * @param x    position
 	 * @param size of mask
 	 * 
-	 * Draws a mask on the specified position
+	 *             Draws a mask on the specified position
 	 */
-	private void drawMask(double y, double x,double size) {
+	private void drawMask(double y, double x, double size) {
 		StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE);
-		StdDraw.setPenRadius(size/4);
-		StdDraw.line(x-0.1, y+0.12, x+0.1, y+0.12);
-		StdDraw.line(x-0.1, y+0.14, x+0.1, y+0.14);
-		StdDraw.line(x-0.1, y+0.16, x+0.1, y+0.16);
-		StdDraw.setPenRadius(size/12);
-		StdDraw.line(x+0.1, y+0.1, x+0.27, y);
-		StdDraw.line(x-0.1, y+0.1, x-0.27, y);
+		StdDraw.setPenRadius(size / 4);
+		StdDraw.line(x - 0.1, y + 0.12, x + 0.1, y + 0.12);
+		StdDraw.line(x - 0.1, y + 0.14, x + 0.1, y + 0.14);
+		StdDraw.line(x - 0.1, y + 0.16, x + 0.1, y + 0.16);
+		StdDraw.setPenRadius(size / 12);
+		StdDraw.line(x + 0.1, y + 0.1, x + 0.27, y);
+		StdDraw.line(x - 0.1, y + 0.1, x - 0.27, y);
 	}
 
 	/**
-	 * This method draws the updated grid every time it is called 
+	 * This method draws the updated grid every time it is called
 	 */
 	public void drawGrid() {
 		double rad;
@@ -185,57 +213,54 @@ public class Room {
 					StdDraw.filledRectangle(x, y, 0.5, 0.5);
 					this.disinfect((int) y, (int) x);
 				}
-				//pen radius is depended on the smallest of width and length
+				// pen radius is depended on the smallest of width and length
 				if (length > width)
 					rad = length;
 				else
 					rad = width;
-				//draws people with the color that matches their status
+				// draws people with the color that matches their status
 				if (isOccupied((int) y, (int) x) != 0) {
 					if (isOccupied((int) y, (int) x) == 1)
 						StdDraw.setPenColor(StdDraw.BLUE);
 					else if (isOccupied((int) y, (int) x) == 2) {
 						StdDraw.setPenColor(StdDraw.RED);
-						possibleInfection((int)y,(int)x);
-					}
-					else if (isOccupied((int) y, (int) x) == 3)
+						possibleInfection((int) y, (int) x);
+					} else if (isOccupied((int) y, (int) x) == 3)
 						StdDraw.setPenColor(StdDraw.GREEN);
 
-					rad=0.255 / (rad * 0.3);
+					rad = 0.255 / (rad * 0.3);
 					StdDraw.setPenRadius(rad);
 					StdDraw.point(x, y);
 				}
-				//draws mask
-				if(takesMeasures((int) y, (int) x))
-					this.drawMask(y, x,rad);
-					
+				// draws mask
+				if (takesMeasures((int) y, (int) x))
+					this.drawMask(y, x, rad);
 
 			}
 		}
 
 		// draw lines
-			StdDraw.setPenColor(StdDraw.GRAY);
-			StdDraw.setPenRadius(0.2 / ((width + length) / 2));
-			for (int y = 0; y <= length; y++) {
-				StdDraw.line(0, y, width, y);
-			}
-			for (int x = 0; x <= width; x++) {
-				StdDraw.line(x, 0, x, length);
-			}
-		
+		StdDraw.setPenColor(StdDraw.GRAY);
+		StdDraw.setPenRadius(0.2 / ((width + length) / 2));
+		for (int y = 0; y <= length; y++) {
+			StdDraw.line(0, y, width, y);
+		}
+		for (int x = 0; x <= width; x++) {
+			StdDraw.line(x, 0, x, length);
+		}
 
 	}
-	
+
 	/**
 	 * 
 	 * @param y position
 	 * @param x position
 	 * 
-	 * This method can possibly infect the specified position when called
-	 * This is dependent of the inf variable and random chance
+	 *          This method can possibly infect the specified position when called
+	 *          This is dependent of the inf variable and random chance
 	 */
 	public void possibleInfection(int y, int x) {
-		if(Randomizer.getBoolean(inf))
+		if (Randomizer.getBoolean(inf))
 			this.infect(y, x);
 	}
 
